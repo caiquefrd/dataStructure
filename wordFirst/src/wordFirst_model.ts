7e48d0338df8f120dd30fe238b59363d4597cd7b
LAB-80 <caique.fred@outlook.com>
2024-03-11 22:05:00 -0300

    att tipagem

--- wordFirst/src/wordFirst_model.ts
+++ wordFirst/src/wordFirst_model.ts
@@ -1,9 +1,9 @@
 class wordFirst {
-  letters: any;
-  constructor(letters: any) {
+  letters:string[] ;
+  constructor(letters:string[]) {
     this.letters = letters;
   }
-  firstWord(input1: any, input2: any):string {
+  firstWord(input1: string, input2: string):string {
     if (this.letters.indexOf(input1.charAt(0)) < this.letters.indexOf(input2.charAt(0))) { //compara a posição da letra extraida dos inputs com o array principal do alfabeto
       return input1;
     } else {
