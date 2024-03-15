b8f1f5be99dd098978e8322f825446a6ffb90c46
Caique Moura <caique.fred@outlook.com>
2024-03-15 11:02:28 -0300

    att org

--- Dictionary/dictionary_view.js
+++ Dictionary/dictionary_view.js
@@ -3,5 +3,6 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Dictionary_model_1 = require("./src/Dictionary_model");
 var promptSync = require("prompt-sync");
 var prompt = promptSync();
-var l = new Dictionary_model_1.default(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
+var alphabet = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+var l = new Dictionary_model_1.default(alphabet);
 console.log("A palavra anterior no dicion\u00E1rio \u00E9: ".concat(l.firstWord(prompt('Entre com a primeira palavra: '), prompt('Entre com a segunda palavra: '))));
--- Dictionary/dictionary_view.ts
+++ Dictionary/dictionary_view.ts
@@ -3,7 +3,8 @@ import * as promptSync from 'prompt-sync'
 
 const prompt = promptSync();
 
+const alphabet:string[] = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
 
-const l = new Dictionary(["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]);
+const l = new Dictionary(alphabet);
 
 console.log(`A palavra anterior no dicionário é: ${l.firstWord(prompt('Entre com a primeira palavra: '), prompt('Entre com a segunda palavra: '))}`);
