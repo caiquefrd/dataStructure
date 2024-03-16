a0e0fcf2cc5045ee98ab1053c14f8b91d3a5cfa4
LAB-80 <caique.fred@outlook.com>
2024-03-15 22:17:40 -0300

    final viavel

--- Dictionary/models/Dictionary_model.js
+++ Dictionary/models/Dictionary_model.js
@@ -5,25 +5,11 @@ var Dictionary = /** @class */ (function () {
         this.letters = letters;
     }
     Dictionary.prototype.firstWord = function (input1, input2) {
-        if (input1 < input2) {
-            for (var i = 0; i < input1.length; i++) {
-                if (this.letters.indexOf(input1.charAt(i)) < this.letters.indexOf(input2.charAt(i))) { //compara a posição da letra extraida dos inputs com o array principal do alfabeto
-                    return input1;
-                }
-                else {
-                    return input2;
-                }
-            }
+        if (this.letters.indexOf(input1.charAt(0)) < this.letters.indexOf(input2.charAt(0))) { //compara a posição da letra extraida dos inputs com o array principal do alfabeto
+            return input1;
         }
         else {
-            for (var i = 0; i < input2.length; i++) {
-                if (this.letters.indexOf(input1.charAt(i)) < this.letters.indexOf(input2.charAt(i))) { //compara a posição da letra extraida dos inputs com o array principal do alfabeto
-                    return input1;
-                }
-                else {
-                    return input2;
-                }
-            }
+            return input2;
         }
     };
     return Dictionary;
--- Dictionary/models/Dictionary_model.ts
+++ Dictionary/models/Dictionary_model.ts
@@ -3,26 +3,13 @@ class Dictionary {
   constructor(letters:string[]) {
     this.letters = letters;
   }
-  firstWord(input1: string, input2: string):any {
-    if ( input1 < input2 ){
-      for ( let i = 0 ; i < input1.length ; i ++ ){
-        if (this.letters.indexOf(input1.charAt(i)) < this.letters.indexOf(input2.charAt(i))) { //compara a posição da letra extraida dos inputs com o array principal do alfabeto
-          return input1;
-        } else {
-          return input2;
-        }
-      }
-    }
-    else {
-      for ( let i = 0 ; i < input2.length ; i ++ ){
-        if (this.letters.indexOf(input1.charAt(i)) < this.letters.indexOf(input2.charAt(i))) { //compara a posição da letra extraida dos inputs com o array principal do alfabeto
-          return input1;
-        } else {
-          return input2;
-        }
+  firstWord(input1: string, input2: string):string {
+    if (this.letters.indexOf(input1.charAt(0)) < this.letters.indexOf(input2.charAt(0))) { //compara a posição da letra extraida dos inputs com o array principal do alfabeto
+      return input1;
+    } else {
+      return input2;
     }
   }
-}
 }
 
 
